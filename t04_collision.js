5ae3fc8e4fa260c33f43cb354db9d4cb414f1f95
Kphilpott <23424kp@hvhs.school.nz>
2026-02-09 13:09:05 +1300

    bounce test

--- t04_collision.js
+++ t04_collision.js
@@ -17,18 +17,20 @@ function setup() {
 
 	bob.rotationSpeed = 2;
     bob.vel.x = 2;
-	
+	bob.bounciness =1
+
 	bill = new Sprite(380, 99, 30, 35, 'd');
     bill.color = 'green';
 	bill.rotationSpeed = -12;
 	bill.vel.x = -6;
+    bill.bounciness = 3
 
 	ben = new Sprite(200, 70, 30, 35, 'd');
     ben.color = 'yellow';
 	ben.rotationSpeed = -61;
 	ben.vel.x = -0.02;
 	ben.vel.y = -0.32;
-
+    ben.bounciness = 19
 
 	platform_1 = new Sprite(100, 100, 5, 900, 'k');
 }
