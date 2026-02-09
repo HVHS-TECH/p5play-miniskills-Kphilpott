1ea50491ddaea9196db05eb9d4fef51849276dde
Kphilpott <23424kp@hvhs.school.nz>
2026-02-09 13:16:53 +1300

    jeb

--- t05_createWalls.js
+++ t05_createWalls.js
@@ -17,20 +17,28 @@ function setup() {
 
 	bob.rotationSpeed = 2;
     bob.vel.x = 2;
-	bob.bounciness = 0.1
+	bob.bounciness = 0.1;
 
 	bill = new Sprite(380, 99, 30, 35, 'd');
     bill.color = 'green';
 	bill.rotationSpeed = -12;
 	bill.vel.x = -6;
-    bill.bounciness = 0.3
+    bill.bounciness = 0.3;
 
 	ben = new Sprite(200, 70, 30, 35, 'd');
     ben.color = 'yellow';
 	ben.rotationSpeed = -61;
 	ben.vel.x = -0.02;
 	ben.vel.y = -0.32;
-    ben.bounciness = 0.9
+    ben.bounciness = 0.9;
+
+	jeb = new Sprite(300, 300, 20);
+	jeb.color = 'purple';
+	jeb.rotationSpeed = 600;
+	jeb.vel.x = 12;
+	jeb.vel.y = 2;
+	jeb.bounciness = 20;
+
 
 	platform_1 = new Sprite(2, 450, 5, 900, 'k');
 	platform_2 = new Sprite(450, 2, 900, 5, 'k');
